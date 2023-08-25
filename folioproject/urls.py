c5d2cb306a28e3000705697f20e1ee0f6e27bc4c
busayo <codedjesus@gmail.com>
2023-08-25 15:03:25 +0100

    admin access update

--- folioproject/urls.py
+++ folioproject/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('busayofolioadmin/', admin.site.urls),
     path('', include('folioappp.urls')),
 ]
 
